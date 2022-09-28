c300518ceea5c031cc86cf2559b25ec892c952f2
sophiemknr <113278987+sophiemknr@users.noreply.github.com>
2022-09-28 19:28:52 +0700

    Add files via upload

---
+++ tugas 3 luas llingkaran.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+  double r, luas;
+  cout << "Masukan jari-jari lingkaran : ";
+  cin >> r;
+  luas = 3.14 * r * r;
+  cout << "Luas lingkaran = "<< luas << endl;
+ 
+  return 0;
+}
