eba983fc3e830a575d1f9afa45c0e5566a4a149b
sophiemknr <113278987+sophiemknr@users.noreply.github.com>
2022-10-12 22:03:25 +0700

    Add files via upload

---
+++ tugas 5 no 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int bil;
+
+  cout << "Program Perulangan Ascending\n";
+  for (bil = 1; bil <=10; bil++)
+    cout << bil << "\n";
+cout<<"\n";
+  cout << "Program Perulangan Descending\n";
+  for (bil = 10; bil >=1; bil--)
+    cout << bil<< "\n";
+  getchar();
+}
